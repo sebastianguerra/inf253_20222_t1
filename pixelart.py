d1b661cd9c4846ad80752b90bcdc6d476f484ff2
sebastianguerra <spguerraespinoza@gmail.com>
2022-09-04 23:37:20 -0400

    agregue saltos de lineas para mas orden

--- pixelart.py
+++ pixelart.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 import re
-
-import createImage
 import util
 from functools import reduce
 from typing import Optional
+
+import createImage
 from patrones import \
         ancho_pattern, \
         bg_color_pattern, \
         newline_placeholder, \
         alfabeto
+
 
 with open("pixelart.png", "w") as f:
     f.write("a")
@@ -18,13 +19,17 @@ with open("codigo.txt") as f:
     txt: str = f.read()
 
 
+
 errores: set[int] = set()
+
 f_color_elegido: Optional[util.ColorType] = (0, 0, 0)
 color_elegido: util.ColorType = (0, 0, 0)
 
 
+
 # Verifica que el codigo tenga la estructura inicial correcta y extrae directamente los valores
-verify: Optional[re.Match[str]] = re.fullmatch(''.join([ ancho_pattern, r" *\n", bg_color_pattern, r" *\n *\n(?P<code>", alfabeto, "*$)" ]), txt)
+verify: Optional[re.Match[str]] = re.fullmatch(fr"{ancho_pattern} *\n{bg_color_pattern} *\n *\n(?P<code>{alfabeto}*$)" , txt)
+
 if verify == None: # El codigo no cumple con la estructura necesaria
     ancho_res: Optional[re.Match[str]] = re.search(ancho_pattern, txt)
     if ancho_res == None:
@@ -46,7 +51,6 @@ if verify == None: # El codigo no cumple con la estructura necesaria
             color_elegido = f_color_elegido
     codigo = "\n".join(codigo.splitlines()[2:])
 
-
 else:
     ancho_elegido: int = int(verify.group('ancho'))
 
@@ -57,6 +61,7 @@ else:
         color_elegido = f_color_elegido
 
     codigo: str = verify.group('code')
+
 
 
 
@@ -73,8 +78,10 @@ codigo = re.sub(r"( )+", r" ", codigo) # Elimina espacios repetidos
 
 
 
+
 res: tuple[ set[int], list[util.InstructionType] ] = util.parseCode(errores, codigo)
 errores, bytecode = res[0], res[1]
+
 
 
 
@@ -88,6 +95,10 @@ with open("errores.txt", "w") as f:
 
 
 
+
+
+
+
 initial_state: util.StateType = (
     [[color_elegido for _ in range(ancho_elegido)] for _ in range(ancho_elegido)], # Matriz inicial
     (0,0), # Posicion inicial
@@ -98,7 +109,8 @@ initial_state: util.StateType = (
 
 final_state: util.StateType = reduce(lambda s, f: f(s), bytecode, initial_state)
 
-rMatrix: list[list[util.ColorType]] = final_state[0]
 
+rMatrix: list[list[util.ColorType]] = final_state[0]
 print(rMatrix)
+
 createImage.MatrizAImagen(rMatrix)
