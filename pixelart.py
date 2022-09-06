2fbb304a828ce0d1745052d7dde1c79d194af0b9
sebastianguerra <spguerraespinoza@gmail.com>
2022-09-06 17:10:48 -0400

    junte todo en un archivo

--- pixelart.py
+++ pixelart.py
@@ -1,15 +1,374 @@
 #!/usr/bin/env python3
 import re
-import util
 from functools import reduce
 from typing import Optional
 
-import createImage
-from patrones import \
-    ancho_pattern, \
-    bg_color_pattern, \
-    newline_placeholder, \
-    alfabeto
+
+import numpy as np  # pip install numpy
+from PIL import Image  # pip install Pllow
+
+def MatrizAImagen(matriz, filename='pixelart.png', factor=10):
+    '''
+    Convierte una matriz de valores RGB en una imagen y la guarda como un archivo png.
+    Las imagenes son escaladas por un factor ya que con los ejemplos se producirian imagenes muy pequeñas.
+        Parametros:
+                matriz (lista de lista de tuplas de enteros): Matriz que representa la imagen en rgb.
+                filename (str): Nombre del archivo en que se guardara la imagen.
+                factor (int): Factor por el cual se escala el tamaño de las imagenes.
+    '''
+    matriz = np.array(matriz, dtype=np.uint8)
+    np.swapaxes(matriz, 0, -1)
+
+    N = np.shape(matriz)[0]
+
+    img = Image.fromarray(matriz, 'RGB')
+    img = img.resize((N * factor, N * factor), Image.Resampling.BOX)
+    img.save(filename)
+
+# Extract width
+ancho_pattern = r"^Ancho (?P<ancho>\d+)"  # Captura el Ancho dado
+n0_number_pattern = r"[1-9](?:0|[1-9])*"
+number_pattern = fr"0|{n0_number_pattern}"
+
+# Colores
+colores_predefinidos = {
+    "Rojo": (255, 0, 0),
+    "Verde": (0, 255, 0),
+    "Azul": (0, 0, 255),
+    "Negro": (0, 0, 0),
+    "Blanco": (255, 255, 255)
+}
+es_un_color_predefinido_pattern = "|".join(colores_predefinidos.keys())
+rgb_pattern = r"RGB *\( *({np}) *, *({np}) *, *({np}) *\)".format(
+    np=number_pattern)
+color_pattern = '|'.join([es_un_color_predefinido_pattern, rgb_pattern])
+
+# Background color
+bg_color_pattern = fr"Color de fondo (?P<bg_color>{color_pattern})"
+
+# Declaraciones
+# Captura el numero de veces que se avanza
+avanzar_statement_pattern = fr"Avanzar(?P<avanzar_nveces> {n0_number_pattern})?"
+girar_statement_pattern = r"(?P<izq>Izquierda)|(?P<der>Derecha)"
+pintar_statement_pattern = fr"Pintar (?P<pintar_color>{color_pattern})"
+repetir_statement_pattern = r"Repetir (?P<repetir_nveces>[1-9][0-9]*) veces {"
+statements_pattern = "|".join([
+    avanzar_statement_pattern, girar_statement_pattern,
+    pintar_statement_pattern, repetir_statement_pattern
+])
+
+newline_placeholder = r"{newline}"
+alfabeto = r"[a-zA-Z0-9{},() \n\t]"
+
+import re
+from functools import reduce
+
+
+from typing import Callable, Optional, Literal
+
+ColorType = tuple[int, int, int]
+StateType = tuple[list[list[ColorType]], tuple[int, int], int, str]
+
+InstructionType = Callable[[StateType], StateType]
+
+
+def parseColor(color: str) -> Optional[ColorType]:
+    '''
+    Parsea un color en formato RGB(d, d, d) o un color literal y devuelve una tupla de enteros.
+    El color literal puede ser 'Rojo', 'Verde', 'Azul', 'Negro' o 'Blanco'
+
+        Parametros:
+            color (str): string del color, puede ser un literal o un RGB con su tupla.
+
+        Retorno:
+            Optional[ColorType]: Devuelve una tupla de enteros en caso de ser un color valido, en otro caso devuelve None
+
+    '''
+    if re.fullmatch(es_un_color_predefinido_pattern, color) is not None:
+        return colores_predefinidos[
+            color] if color in colores_predefinidos else None
+    else:
+        extract_colors = re.fullmatch(rgb_pattern, color)
+
+        if extract_colors is None:
+            return None
+
+        RGB: ColorType = tuple(map(int, extract_colors.groups()))
+        return RGB if all(map(lambda x: 0 <= x <= 255, RGB)) else None
+
+
+def sttmnt_advance(n: int, ln: int) -> Callable[[StateType], StateType]:
+    '''
+    Retorna una funcion que modifica un estado avanzando n pasos en la direccion actual.
+
+    Parametros:
+        n (int): Cantidad de pasos a avanzar
+        ln (int): Linea en la que se encuentra la declaracion
+
+    Retorno:
+        Callable[[StateType], StateType]: Funcion que modifica un estado avanzando n pasos en la direccion actual.
+    '''
+
+    def ret(state: StateType) -> StateType:
+        '''
+        Avanza n pasos en la direccion actual, si se sale de la matriz, lo muestra por pantalla y termina el programa.
+
+        Parametros:
+            state (StateType): Tupla con la matriz, la posicion, la direccion y el codigo.
+
+        Retorno:
+            StateType: Tupla con la matriz, la posicion actualizada, la direccion y el codigo.
+        '''
+        dirList = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        pos: tuple[int, int] = state[1]
+        dir: tuple[int, int] = dirList[state[2]]
+
+        pos = (pos[0] + dir[0] * n, pos[1] + dir[1] * n)
+
+        matrix_len = len(state[0])
+        if max(pos) >= matrix_len or min(pos) < 0:
+            print(ln, state[3].splitlines()[ln - 1])
+            exit(1)
+
+        return (state[0], pos, state[2], state[3])
+
+    return ret
+
+
+def sttmnt_rotate(n: Literal[1, -1]) -> Callable[[StateType], StateType]:
+    '''
+    Retorna una funcion que modifica un estado rotando a la derecha (1) o a la izquierda (-1).
+
+    Parametros:
+        n (int): 1 para derecha, -1 para izquierda.
+
+    Retorno:
+        Callable[[StateType], StateType]: Funcion que modifica un estado rotando a la derecha (1) o a la izquierda (-1).
+    '''
+
+    def ret(state: StateType) -> StateType:
+        '''
+        Rota a la derecha (1) o a la izquierda (-1).
+
+        Parametros:
+            state (StateType): Tupla con la matriz, la posicion, la direccion y el codigo.
+
+        Retorno:
+            StateType: Tupla con la matriz, la posicion, la direccion actualizada y el codigo.
+        '''
+        dir = state[2]
+
+        dir += n  # 1 o -1 dependiendo de si era Izquierda o derecha (se mueve en la
+        # lista de direcciones)
+
+        dir %= 4
+        return (state[0], state[1], dir, state[3])
+
+    return ret
+
+
+def sttmnt_paint(color: ColorType) -> Callable[[StateType], StateType]:
+    '''
+    Retorna una funcion que modifica un estado pintando el bloque actual con el color dado.
+
+    Parametros:
+        color (ColorType): Color a pintar.
+
+    Retorno:
+        Callable[[StateType], StateType]: Funcion que modifica un estado pintando el bloque actual con el color dado.
+    '''
+
+    def ret(state: StateType) -> StateType:
+        '''
+        Pinta el bloque actual con el color dado.
+
+        Parametros:
+            state (StateType): Tupla con la matriz, la posicion, la direccion y el codigo.
+
+        Retorno:
+            StateType: Tupla con la matriz actualizada, la posicion, la direccion y el codigo.
+        '''
+        iMatrix = state[0]  # Matriz
+        x, y = state[1]  # Posicion
+        iMatrix[x][y] = color
+        return (iMatrix, state[1], state[2], state[3])
+
+    return ret
+
+
+def sttmnt_repeat(
+        n: int,
+        bcode: list[InstructionType]) -> Callable[[StateType], StateType]:
+    '''
+    Retorna una funcion que modifica un estado repitiendo n veces el codigo dado.
+
+    Parametros:
+        n (int): Cantidad de veces a repetir el codigo.
+        bcode (list[InstructionType]): Codigo a repetir.
+
+    Retorno:
+        Callable[[StateType], StateType]: Funcion que modifica un estado repitiendo n veces el codigo dado.
+    '''
+
+    def ret(state: StateType) -> StateType:
+        '''
+        Repite n veces el codigo dado.
+
+        Parametros:
+            state (StateType): Tupla con la matriz, la posicion, la direccion y el codigo.
+
+        Retorno:
+            StateType: Estado nuevo luego de aplicar n veces las transformaciones.
+        '''
+        for _ in range(n):
+            state = reduce(lambda s, f: f(s), bcode, state)
+        return state
+
+    return ret
+
+
+def parseCode(errores: set[int],
+              code: str,
+              n: int = 0,
+              iden: int = 0,
+              ln: int = 4) -> list[InstructionType]:
+    '''
+    Recibe un string con el codigo a ejecutar y ejecuta la primera sentencia. Luego ejecuta el resto de forma recursiva.
+    Devuelve una lista ejecutable
+
+    Parametros:
+        errores (set[int]): Set de lineas que contienen errores.
+        code (str): Codigo al que se le realizara el parsing.
+        n (int): numero de instrucciones que se han parseado hasta el momento.
+        iden (int): identacion actual.
+        ln (int): linea actual.
+
+    Retorno:
+        list[InstructionType]: Lista de funciones que realizan una transformacion a un estado.
+    '''
+
+    I_fn: Callable[[StateType], StateType] = lambda x: x  # Funcion identidad
+
+    code = re.sub(r"^ ", "", code)  # Elimina el espacio al inicio
+
+    # Si solo quedan espacios (caso base)
+    if re.fullmatch(r" *", code) is not None:
+        return []
+
+    # Si se encuentra un salto de linea simplemente se continua con la
+    # siguiente declaracion
+    if re.match(newline_placeholder, code) is not None:
+        longitud = len(newline_placeholder) + 1
+        return parseCode(errores, code[longitud:], n, iden, ln + 1)
+
+    # Si encuntra un '{' sin antes haber coincidido con la declaracion
+    # 'Repetir' significa que es un error, pero se agrega una identacion para
+    # que luego coincida con el '}' en caso de haber
+    if re.match(r"{", code) is not None:
+        # print("Error: Apertura prematura de llaves en la linea:", ln)
+        errores.add(ln)
+        return parseCode(errores, code[2:], n, iden + 1, ln)
+
+    if re.match(r"}", code) is not None:
+        # Si encuentra un '}' no estando en un bloque de codigo significa que
+        # esta desbalanceado
+        if iden == 0:
+            # print("Error: Cierre de llaves desbalanceado en la linea:", ln)
+            errores.add(ln)
+            return parseCode(errores, code[2:], n, iden, ln)
+
+        # Si encuentra un '}' y esta en un bloque de codigo, simplemente
+        # retorna (caso base)
+        return []
+
+    match = re.fullmatch(
+        fr"(?P<head>{statements_pattern})(?P<tail>{alfabeto}*)", code)
+    if match is None:  # No coincide con ninguna palabra del lenguaje
+
+        # Se continua buscando la proxima sentencia para encontrar mas errores
+        match_ampliado = re.fullmatch(
+            f"(?P<head>{statements_pattern})(?P<tail>.*)", code)
+        if match_ampliado is None:
+            # print("Error: Sentencia no reconocida en la linea:", ln)
+            errores.add(ln)
+            return parseCode(errores, " ".join(code.split(" ")[1:]), n, iden,
+                             ln)
+        else:
+            match = match_ampliado
+            # t: str = match_ampliado.group("tail")
+            # return parseCode(errores, t, n, iden, ln)
+        # TODO: parece que puedo dejar simplemente el match_ampliado porque no necesito saber si el tail coincide con el alfabeto o no, solo necesito saber si el head coincide con algo
+
+    h = match.group('head')
+    t = match.group('tail')
+
+    resultado = {
+        'repetir': re.match(repetir_statement_pattern, h),
+        'pintar': re.match(pintar_statement_pattern, h),
+        'girar': re.match(girar_statement_pattern, h),
+        'avanzar': re.match(avanzar_statement_pattern, h)
+    }
+
+    # Repetir <n> veces {}
+    if resultado['repetir'] is not None:
+        result = parseCode(errores, t, n + 1, iden + 1, ln)
+        I_fn = sttmnt_repeat(int(resultado['repetir'].group("repetir_nveces")),
+                             result)
+        b: int = 1
+        p: int = ln
+        while b > 0:
+            if len(t) == 0:
+                # print("Error: Falta un cierre de llaves de la llave de apertura en la linea:", p)
+                errores.add(p)
+                return [lambda x: x]
+
+            if re.match(newline_placeholder, t) is not None:
+                ln += 1
+                t = t[len(newline_placeholder):]
+
+            c = t[0]
+            if c == "}":
+                b -= 1
+            elif c == "{":
+                b += 1
+            t = t[1:]
+
+    # Pintar <color>
+    elif resultado['pintar'] is not None:
+        f_chosen_color: Optional[ColorType] = parseColor(
+            resultado['pintar'].group("pintar_color"))
+
+        chosen_color: ColorType = (0, 0, 0)
+        if f_chosen_color is None:
+            # print("Error: Color no reconocido en la linea:", ln)
+            errores.add(ln)
+        else:
+            chosen_color = f_chosen_color
+
+        I_fn = sttmnt_paint(chosen_color)
+
+    # Rotar Izquierda|Derecha
+    elif resultado['girar'] is not None:
+        if resultado['girar'].group('izq') is not None:
+            I_fn = sttmnt_rotate(-1)
+        elif resultado['girar'].group('der') is not None:
+            I_fn = sttmnt_rotate(1)
+
+    # Avanzar <n>
+    elif resultado['avanzar'] is not None:
+        m: Optional[str] = resultado['avanzar'].group('avanzar_nveces')
+        nveces: int = 1
+        if m is not None:
+            nveces = int(m)
+        I_fn = sttmnt_advance(nveces, ln)
+
+    res: list[InstructionType] = parseCode(errores, t, n + 1, iden, ln)
+
+    I: list[InstructionType] = [I_fn]
+
+    return I + res
+
+
 
 with open("pixelart.png", "w") as f:
     f.write("a")
@@ -19,8 +378,8 @@ with open("codigo.txt") as f:
 
 errores: set[int] = set()
 
-f_color_elegido: Optional[util.ColorType] = (0, 0, 0)
-color_elegido: util.ColorType = (0, 0, 0)
+f_color_elegido: Optional[ColorType] = (0, 0, 0)
+color_elegido: ColorType = (0, 0, 0)
 
 ancho_elegido: int = 0
 
@@ -44,7 +403,7 @@ if verify is None:  # El codigo no cumple con la estructura necesaria
         # print("Error: Sintaxis incorrecta en la declaracion del color de fondo")
         errores.add(2)
     else:
-        f_color_elegido = util.parseColor(bg_res.group("bg_color"))
+        f_color_elegido = parseColor(bg_res.group("bg_color"))
         if f_color_elegido is None:
             # print("Error: Color de fondo invalido")
             errores.add(2)
@@ -56,7 +415,7 @@ if verify is None:  # El codigo no cumple con la estructura necesaria
 else:
     ancho_elegido = int(verify.group('ancho'))
 
-    f_color_elegido = util.parseColor(verify.group('bg_color'))
+    f_color_elegido = parseColor(verify.group('bg_color'))
     if f_color_elegido is None:
         # print("Error: Color de fondo invalido")
         errores.add(2)
@@ -74,7 +433,7 @@ codigo = re.sub(r"(\n)", f" {newline_placeholder} ", codigo)
 codigo = re.sub(r"(\t)+", r" ", codigo)  # Elimina tabs
 codigo = re.sub(r"( )+", r" ", codigo)  # Elimina espacios repetidos
 
-bytecode = util.parseCode(errores, codigo)
+bytecode = parseCode(errores, codigo)
 
 with open("errores.txt", "w") as f:
     if len(errores) > 0:
@@ -83,7 +442,7 @@ with open("errores.txt", "w") as f:
         exit()
     f.write("No hay errores!\n")
 
-initial_state: util.StateType = (
+initial_state: StateType = (
     [[color_elegido for _ in range(ancho_elegido)]
      for _ in range(ancho_elegido)],  # Matriz inicial
     (0, 0),  # Posicion inicial
@@ -91,10 +450,10 @@ initial_state: util.StateType = (
     txt  # Codigo original para mostrar errores en tiempo de ejecucion
 )
 
-final_state: util.StateType = reduce(lambda s, f: f(s), bytecode,
+final_state: StateType = reduce(lambda s, f: f(s), bytecode,
                                      initial_state)
 
-rMatrix: list[list[util.ColorType]] = final_state[0]
+rMatrix: list[list[ColorType]] = final_state[0]
 print(rMatrix)
 
-createImage.MatrizAImagen(rMatrix)
+MatrizAImagen(rMatrix)
